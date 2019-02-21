6d75f1029931b0e14c0d10b18e1ec17736cc6a42
Maksim V <maks112v@gmail.com>
2019-02-21 15:27:11 -0500

    Converted Search

--- instagram/src/components/SearchBar/SearchBar.jsx
+++ instagram/src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,48 @@
 import React from 'react'
 import './SearchBar.css';
+import styled from 'styled-components'
+
+const HeaderDiv = styled.div`
+  background-color: white;
+  border-bottom: 1px solid #EDF2F9;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  z-index: 10000;
+`;
+
+const Header = styled.header`
+  display: flex;
+  align-items: center;
+  height: 60px;
+  width: 80%;
+  margin: auto;
+  justify-content: space-between; 
+`;
+
+const SearchInput = styled.input`
+  border-radius: 5px;
+  border: 1px solid #EDF2F9;
+  height: 30px;
+  font-size: 15px;
+  text-align: center;
+
+  &:focus{
+    outline: none;
+    border-color: #2C7BE5;
+  }
+`;
+
 
 const SearchBar = props => {
   return(
-  <div className="header">
-    <header>
+  <HeaderDiv>
+    <Header>
       <div><i className="fab fa-instagram fa-lg"></i> Instagram</div>
-      <div><input type="text" placeholder="Search" value={props.searchValue} onChange={props.searchHandler} /></div>
+      <div><SearchInput type="text" placeholder="Search" value={props.searchValue} onChange={props.searchHandler} /></div>
       <div><i className="fas fa-compass icon fa-lg"></i><i className="far fa-heart icon fa-lg"></i><i onClick={props.logOutHandler} className="fas fa-user-alt fa-lg"></i></div>
-    </header>
-  </div>
+    </Header>
+  </HeaderDiv>
   );
 }
 
